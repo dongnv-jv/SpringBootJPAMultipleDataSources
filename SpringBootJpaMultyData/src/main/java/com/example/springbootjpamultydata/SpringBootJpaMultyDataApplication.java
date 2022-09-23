9647560e67ed45211099725c959f30489d035422
dongbk97 <dongtr2202@gmail.com>
2022-09-23 14:06:11 +0700

    Create project

---
+++ SpringBootJpaMultyData/src/main/java/com/example/springbootjpamultydata/SpringBootJpaMultyDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpamultydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMultyDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaMultyDataApplication.class, args);
+    }
+
+}
---
+++ SpringBootJpaMultyData/src/test/java/com/example/springbootjpamultydata/SpringBootJpaMultyDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpamultydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaMultyDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
